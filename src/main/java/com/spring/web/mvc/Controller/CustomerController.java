eee6d31ddf131e245b806380cf4ff8e8650d7ff0
adnanrahin <adnan.rahin13@gmail.com>
2020-01-14 14:09:32 -0500

    update

--- src/main/java/com/spring/web/mvc/Controller/CustomerController.java
+++ src/main/java/com/spring/web/mvc/Controller/CustomerController.java
@@ -3,17 +3,31 @@ package com.spring.web.mvc.Controller;
 import com.spring.web.mvc.Model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
     @RequestMapping("/showForm")
-    public String process(Model model){
+    public String process(Model model) {
         model.addAttribute("customer", new Customer());
         return "customerLogin";
     }
 
+
+    @RequestMapping("/process")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "customerLogin";
+        }
+
+        return "customer-form-view";
+    }
 
 }
---
+++ src/main/webapp/WEB-INF/view/customer-form-view.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: adnanrahin
+  Date: 1/14/20
+  Time: 1:33 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Customer Form View</title>
+</head>
+<body>
+
+First Name: ${customer.firstName}
+<br><br>
+Last Name: ${customer.lastName}
+
+</body>
+</html>
