f6c286992a9c76c2c33024ca8694ed3fbcbb129b
adnanrahin <adnan.rahin13@gmail.com>
2020-01-14 16:23:35 -0500

    @initbinder

--- src/main/java/com/spring/web/mvc/Controller/CustomerController.java
+++ src/main/java/com/spring/web/mvc/Controller/CustomerController.java
@@ -4,14 +4,24 @@ import com.spring.web.mvc.Model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @InitBinder
+    public void preProcess(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String process(Model model) {
