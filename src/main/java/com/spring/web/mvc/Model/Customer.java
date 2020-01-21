ae86620f29d622ce06bdc88ba0a3bff64511e49f
Arx1971 <adnan.rahin13@gmail.com>
2020-01-20 17:37:14 -0800

    Regex Validation

--- src/main/java/com/spring/web/mvc/Model/Customer.java
+++ src/main/java/com/spring/web/mvc/Model/Customer.java
@@ -11,10 +11,11 @@ public class Customer {
     private String lastName;
 
     // Adding Validation Rules for
-
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
+
     @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars / digits")
     private String postalCode;
 
@@ -37,11 +38,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
